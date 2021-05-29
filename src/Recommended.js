826c740cc9ff0bbabaf3becc07241ae8b94e4210
kazukionodera <derazukihiko@gmail.com>
2021-05-29 11:57:16 +0900

    fifth commit

--- src/Recommended.js
+++ src/Recommended.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './Recommended.css'
 
 const Recommended = () => {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] =useState([])
+
+  const addTodo = content => {
+    setTodos([
+      ...todos,
+      {
+        content:content
+      }
+    ])
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if ( value =='') {
+      alert('入力してください');
+      return false;
+    } else {
+    }
+    addTodo(value)
+    setValue('')
+  }
+
   return(
     <div>
       <h1>おすすめ機能</h1>
@@ -23,6 +46,17 @@ const Recommended = () => {
         </TabList>
         <TabPanel>
           <h2>CodeVillage</h2>
+          <div addTodo={addTodo} />
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              onChange={e => {
+                setValue(e.target.value)
+                console.log(e.target.value)
+              }}
+              value={value}
+              />
+          </form>
         </TabPanel>
         <TabPanel>
           <h2>寺崎おすすめ</h2>
@@ -34,6 +68,7 @@ const Recommended = () => {
           <h2>小野寺おすすめ</h2>
         </TabPanel>    
       </Tabs>
+
     </div>
   )
 }
